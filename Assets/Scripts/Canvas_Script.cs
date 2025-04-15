ef2c38553ddef6fb30fcf8869ae3b183ba78ebc5
goalgoloo1 <goalgoloo1@gmail.com>
2025-04-15 12:51:50 +0900

    init

---
+++ Assets/Scripts/Canvas_Script.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Canvas_Script : MonoBehaviour
+{
+    public GameObject lowHp_UI;
+    public GameObject gameOver;
+    public GameObject score;
+    public GameObject staminaBar;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
